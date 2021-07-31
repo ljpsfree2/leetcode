608e4016c46565d5841ec33ba32522d9360b35d6
vcf蔡峰 <caif@trip.com>
2021-07-31 11:21:19 +0800

    REV: add xorsum code.

---
+++ pkg/tasks/xorsum.go
@@ -0,0 +1,23 @@
+package tasks
+
+import "fmt"
+
+var result int
+var nums []int
+
+func dfs(val int, idx int) {
+	if idx == len(nums) {
+		// 退出递归
+		result = result + val
+		return
+	}
+	dfs(val^nums[idx], idx+1)
+	dfs(val, idx+1)
+}
+
+func SubsetXORSum(arr []int) int {
+	nums = arr
+	dfs(0, 0)
+	fmt.Println(result)
+	return result
+}
---
+++ pkg/tasks/xorsum_test.go
@@ -0,0 +1,13 @@
+package tasks
+
+import (
+	"testing"
+)
+
+func TestSubsetXorSum(t *testing.T) {
+	nums = []int{5, 1, 6}
+	result := SubsetXORSum(num)
+	if result != 321 {
+		t.Fatalf("num: 123, expected 321, result: %d", result)
+	}
+}
